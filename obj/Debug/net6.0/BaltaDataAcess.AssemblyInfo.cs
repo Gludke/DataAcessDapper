e26efb7ca46a0323c73a29fa2943219ceb974984
Guilherme Ludke <guiludke@gmail.com>
2024-02-18 15:48:11 -0300

    finalizacao

--- obj/Debug/net6.0/BaltaDataAcess.AssemblyInfo.cs
+++ obj/Debug/net6.0/BaltaDataAcess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaltaDataAcess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a2072f405db861a3741334040cd992d2867125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+599be80d61a4309362654b4e520626fcba12e917")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaltaDataAcess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaltaDataAcess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
